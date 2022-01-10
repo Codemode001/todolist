7983c7f05747dd2540af5135c74687c995c35025
Kimoy Catayas <elishakimcatayas@gmail.com>
2022-01-10 21:07:51 +0800

    Fix error in styling and add material ui

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-
-import Home from './components/home'
-import ToDo from './components/todolist'
-
+import Home from "./components/home";
+import ToDo from "./components/todolist";
 
 const App = () => {
-    return (
-        <div className='App-Main'>
-            <Switch>
-                
-                <Route path = '/home'>
-                    <Home />
-                </Route>
-                <Route path = '/todo'>
-                    <ToDo />
-                </Route>
-                <Route path = '/'>
-                    <Home />
-                </Route>
-
-            </Switch>
-
-        </div>
-    )
-
-}
-export default App
+  return (
+    <div className="App-Main">
+      <Switch>
+       <Route path="/todo">
+          <ToDo />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  );
+};
+export default App;
--- src/components/todolist.js
+++ src/components/todolist.js
@@ -1,11 +1,6 @@
-import React from 'react'
+import React from "react";
 
 const ToDo = () => {
-
-    return (
-        <div className='todo-main'>
-            You are in ToDo list
-        </div>
-    )
-}
-export default ToDo
+  return <div className="todo-main">You are in ToDo list</div>;
+};
+export default ToDo;
